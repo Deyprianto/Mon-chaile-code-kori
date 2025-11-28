b70d38d4177c0af14c5cbd3a28ef7fc7a753ccad
prianto dey <priantodey12@gmail.com>
2025-11-29 00:14:33 +0600

    simple array list

---
+++ ArrayListMain.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListMain{
+    public static void main(String[] args) {
+        ArrayList<String> cars  = new ArrayList<>();
+        cars.add("walton");
+        cars.add("noah");
+        cars.add("cng");
+        cars.add("rickshaw");
+        System.out.println(cars);
+
+    }
+}
