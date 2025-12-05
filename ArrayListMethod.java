34519d0151950a6c37ab9d3c8af26a729dd44a9d
prianto dey <priantodey12@gmail.com>
2025-12-05 23:00:29 +0600

    array list method implimantation

---
+++ ArrayListMethod.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+public class ArrayListMethod {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("pronoy");
+        names.add("das");
+        names.add("mohon");
+        System.out.println(names.get(1));
+        names.set(2,"brommochari");
+        System.out.println(names);
+        names.remove(2);
+        System.out.println(names);
+        names.set(1,"demon");
+        System.out.println(names);
+        
+    }
+}
