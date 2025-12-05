93128df5c4d1d1293ba09a645e8762fddb80fe25
prianto dey <priantodey12@gmail.com>
2025-12-05 18:56:35 +0600

    hash map code simple

---
+++ HashMapDemo.java
@@ -0,0 +1,13 @@
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String,String> names = new HashMap<String,String>();
+        names.put("anik", "flutter");
+        names.put("tamim", "django");
+        names.put("mutahar", "web");
+
+        System.out.println(names);
+    }
+}
