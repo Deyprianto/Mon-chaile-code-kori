76a9e5a6b9d7578a8bcafee9698cf668faa2e283
prianto dey <priantodey12@gmail.com>
2025-12-05 18:47:21 +0600

    array list simple

---
+++ SimpleArrayList.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+
+public class SimpleArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("shoily");
+        names.add("kanta");
+        names.add("prity");
+        names.add("barna");
+        names.add("shova");
+
+        System.out.println(names);
+        //System.out.println(names.get(2));
+    }
+   
+}
