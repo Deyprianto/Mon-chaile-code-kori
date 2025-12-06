99a4c5fee6a79f68d5cb91b232196116d60b391c
prianto dey <priantodey12@gmail.com>
2025-12-06 13:21:20 +0600

    List and ArrayList

---
+++ ArrayListVar.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class ArrayListVar {
+    public static void main(String[] args) {
+        var nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        System.out.println(nums);
+    }
+}
---
+++ ListArrayList.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListArrayList {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<String>();
+        names.add("jodu");
+        names.add("modu");
+        names.add("kodu");
+
+        System.out.println(names);
+        
+    }
+}
