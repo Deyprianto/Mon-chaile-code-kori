b319602793c1230ccd02ccd895d16a5e2d86605a
prianto dey <priantodey12@gmail.com>
2025-12-06 21:51:40 +0600

    Linked List

---
+++ LinkedListDemo.java
@@ -0,0 +1,18 @@
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        System.out.println(nums);
+        nums.addFirst(10);
+        nums.addLast(20);
+        System.out.println(nums);
+
+    }
+}
