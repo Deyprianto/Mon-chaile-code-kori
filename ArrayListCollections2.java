8a572222aef6bb79c7406603302293a1f963efde
prianto dey <priantodey12@gmail.com>
2025-12-09 01:36:52 +0600

    collections arraylist sort korechi

---
+++ ArrayListCollections2.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListCollections2 {
+    //sort by number
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(10);
+        nums.add(2);
+        nums.add(1);
+        nums.add(5);
+        nums.add(8);
+        nums.add(0);
+        Collections.sort(nums);
+
+        for(Integer i:nums){
+            System.out.println(i);
+        }
+    }
+}
