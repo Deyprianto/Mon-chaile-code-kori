a6c6e7ac2c6bbf6c812d7fbcd4a863e40699d6a1
prianto dey <priantodey12@gmail.com>
2025-12-13 01:25:31 +0600

    Hashmap basics

---
+++ HashMapBasic.java
@@ -0,0 +1,19 @@
+
+import java.util.HashMap;
+
+public class HashMapBasic {
+    public static void main(String[] args) {
+        HashMap<String,String> countryCity = new HashMap<String,String>();
+        countryCity.put("England","London");
+        countryCity.put("India","Delhi");
+        countryCity.put("Bangladesh","Dhaka");
+        countryCity.put("USA","california");
+        countryCity.put("China","beijing");
+        countryCity.put("Australia","brisbane");
+
+        System.out.println(countryCity);
+        countryCity.remove("Australia");
+        System.out.println(countryCity);
+
+    }
+}
